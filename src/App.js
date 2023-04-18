ed8ff95c49df23c7acaaa0faa26ab103fba4451c
SaidGM <said.mg.1517@gmail.com>
2023-04-18 23:30:46 +0200

    formAdmin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './Assets/App.css';
 import Navbar from './Components/navbar/Navbar';
-import Home from './Pages/Home/Home';
-import Info from './Pages/Info/Info';
-import LogIn from './Pages/LogIn/LogIn';
-import Register from './Pages/Register/Register';
+// import Home from './Pages/Home/Home';
+// import Info from './Pages/Info/Info';
+// import LogIn from './Pages/LogIn/LogIn';
+// import Register from './Pages/Register/Register';
+import Admin from './Pages/Admin/Admin';
+import AddForm from './Pages/AddForm/AddForm';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route path='/login' element={<LogIn/>}/> 
-        <Route path='/register' element={<Register/>}/> 
-        <Route path='/' element={<Home/>}/> 
-        <Route path='/infoEvent/:id' element={<Info/>}/>
-        <Route path='/admin' element={<Info/>}/>
-        <Route path='/form' element={<Info/>}/>
+        {/* <Route path='/login' element={<LogIn/>}/>  */}
+        {/* <Route path='/register' element={<Register/>}/>  */}
+        {/* <Route path='/' element={<Home/>}/>  */}
+        {/* <Route path='/infoEvent/:id' element={<Info/>}/> */}
+        <Route path='/' element={<Admin/>}/>
+        <Route path='/form' element={<AddForm/>}/>
       </Routes>
     </> 
   ); 
