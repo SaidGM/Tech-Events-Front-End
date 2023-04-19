62afa1fdaabea9bb343902277c06d79f87b955e4
SaidGM <said.mg.1517@gmail.com>
2023-04-19 16:23:26 +0200

    updat

--- src/Components/formAdd/FormAdd.jsx
+++ src/Components/formAdd/FormAdd.jsx
@@ -49,9 +49,8 @@ function FormAdd() {
             </select>
         </div>
         <div className='containerFlex containerFile'>          
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file"/>
-            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+            <label htmlFor="">Url de la imagen:</label>
+            <input type="file" name=''/>
         </div>
         <div className='buttonAddEvent'>
             <button type='submit'>Añadir Evento</button>
