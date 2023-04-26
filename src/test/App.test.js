fa4083b06fbac98f2d899e1163ddf9cb29b9ac5c
SaidGM <said.mg.1517@gmail.com>
2023-04-26 11:16:57 +0200

    branchFinal

---
+++ src/test/App.test.js
@@ -0,0 +1,11 @@
+import App from '../App'
+import { render, screen } from '@testing-library/react'
+
+describe('', () => {
+    beforeEach(()=> render(<App/>))
+    test('renders main page correctly', () => {
+        screen.debug();
+        espect(true).toBeTruthy()
+    })
+    
+})
